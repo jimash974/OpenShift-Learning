74b601845ff35ec632982dd4915e86aa48f8e099
jimash974 <jeremynchristopher@gmail.com>
2025-06-05 13:49:14 +0700

    fixing bug semicolon in authcontroller

--- src/main/java/com/bni/bni/controller/AuthController.java
+++ src/main/java/com/bni/bni/controller/AuthController.java
@@ -43,7 +43,7 @@ public class AuthController {
         if (token != null) {
             response.put("status", 200);
             response.put("token", token);
-            response.put("message", "Login Berhasil")
+            response.put("message", "Login Berhasil");
             return ResponseEntity.ok(response);
         } else {
             response.put("status", 401);
